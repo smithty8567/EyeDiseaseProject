ae28bf91ab7b0f9b1a57dd3ceb8896499367b4f0
jthering234156789 <jthering225@gmail.com>
2025-05-04 13:58:34 -0500

    added canny comments

--- canny.py
+++ canny.py
@@ -1,11 +1,19 @@
 import numpy as np
 import cv2
 import torch
+
 def addCannyLayer(img_tensor):
-    # img_tensor: [3, H, W], float32
-    np_img = img_tensor.numpy().transpose(1, 2, 0)  # [H, W, 3]
-    gray = np.clip((np_img * 255), 0, 255).astype(np.uint8)
-    gray = cv2.cvtColor(gray, cv2.COLOR_RGB2GRAY)
+    #transform tensor image to proper image
+    np_img = img_tensor.numpy().transpose(1, 2, 0)
+
+    #grayscale image
+    gray = cv2.cvtColor(np_img, cv2.COLOR_RGB2GRAY)
+
+    #do edge detection on grayscale image
     edges = cv2.Canny(gray, 0, 15).astype(np.float32) / 255.0
-    edge_tensor = torch.from_numpy(edges).unsqueeze(0)  # [1, H, W]
-    return torch.cat((img_tensor, edge_tensor), dim=0)  # [4, H, W]
+
+    #convert our numpy image to a tensor image
+    edge_tensor = torch.from_numpy(edges).unsqueeze(0)
+
+    #return new edge channel with original image
+    return torch.cat((img_tensor, edge_tensor), dim=0)
